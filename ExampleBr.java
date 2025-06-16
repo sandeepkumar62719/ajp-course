0ef1b47411b2975ea50d5e471ffc55347c3f515b
SANDEEP KUMAR .MANNEKUNTA <sandeepkumar62719@gmail.com>
2025-06-16 15:54:52 +0530

    Add files via upload

---
+++ ExampleBr.java
@@ -0,0 +1,11 @@
+public class ExampleBr {
+public static void main(String args[]){
+int num=10;
+while (num>0){
+if (num==5) break;
+System.out.println(num);
+num--;
+}
+System.out.println("loop Exited");
+}
+}
